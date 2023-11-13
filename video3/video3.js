28642b925bc3fd464cd6764cfb1262c482440974
Kang <vancong171001@gmail.com>
2023-11-13 23:19:04 +0700

    chapter 2, video 4

--- video3/video3.js
+++
@@ -1,3 +0,0 @@
-let x = 5, y = 6, z = 7;
-
-console.log("Test string: ", "x =", x,"y =", y, "z =", z);
---
+++ video4/video4.js
@@ -0,0 +1,6 @@
+let a = `I'am a string`;
+let text = `We are the so-called
+"Vikings" from the north.`;
+
+console.log(text);
+console.log(a);
