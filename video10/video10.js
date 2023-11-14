24cb301d69b9d0e59d52f06c048c0e62a62e6226
Kang <vancong171001@gmail.com>
2023-11-14 18:30:07 +0700

    chapter 2, video 10

---
+++ video10/video10.js
@@ -0,0 +1,18 @@
+let a = 5;
+let b = '5';
+
+// === : type, value convention
+console.log('Compare a == b :', 'a =', a, 'b =', b, 'a == b', a == b);
+console.log('Compare a === b :', 'a =', a, 'b =', b, 'a == b', a === b);
+
+// Emty
+// let c = {};
+// console.log('check value:', c);
+
+// null
+// let c = null;
+// console.log('check value:', c);
+
+// undefined
+// let c;
+// console.log('check value:', c);
