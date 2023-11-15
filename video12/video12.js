e5cc1b359f8d448e5ad5824112059b51299e7522
Kang <vancong171001@gmail.com>
2023-11-15 11:04:49 +0700

    chapter 3, video 2

---
+++ video12/video12.js
@@ -0,0 +1,14 @@
+let arrTop4 = ['ManCity', 'Liver', 'Mu', 'Chelsea'];
+
+let i = 0;
+// while (i < arrTop4.length) {
+//     console.log('Top:', i + 1, arrTop4[i]);
+//     i++;
+// }
+
+let flag = false;
+while (!flag) {
+    console.log('Check value: ', i);
+    i++;
+    if (i === 10) flag = true;  
+}
